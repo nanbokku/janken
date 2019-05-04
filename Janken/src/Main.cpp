60ecd5f520de85d7ae12c17dd336c3dcd0aecd31
nanbokku <n1510126@edu.cc.uec.ac.jp>
2019-05-04 15:42:15 +0900

    [add] Add this project.

---
+++ Janken/src/Main.cpp
@@ -0,0 +1,7 @@
+﻿
+#include <Siv3D.hpp> // OpenSiv3D v0.3.1
+
+void Main()
+{
+	
+}
---
+++ Janken/src/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Constants
+{
+	const enum class SceneName
+	{
+		TITLE,
+		PLAY,
+		RESULT
+	};
+}

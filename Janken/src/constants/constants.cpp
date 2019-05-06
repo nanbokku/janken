e7541d969bc03782e8f567c578a6bede6cc94bbb
nanbokku <n1510126@edu.cc.uec.ac.jp>
2019-05-06 14:38:58 +0900

    [change] Move constants.h to constants directory.

---
+++ Janken/src/constants/constants.cpp
@@ -0,0 +1,6 @@
+#include "constants.h"
+
+using namespace Constants;
+
+const String Title::TitleStr = String(U"後だしジャンケン");
+const String Title::StartBtnStr = String(U"スタート");
---
+++ Janken/src/constants/constants.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Siv3D.hpp>
+
+namespace Constants
+{
+	const enum class Scene
+	{
+		Title,
+		Play,
+		Result
+	};
+
+	const enum class GameState
+	{
+		Countdown,
+		Play
+	};
+
+	constexpr Size WindowSize(800, 600);
+
+	struct Title
+	{
+		static const String TitleStr;
+		static const String StartBtnStr;
+	};
+}

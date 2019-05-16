c2397cd40bd2c86e011a051380eac20fb2b23e7a
Yuka Namboku <29055086nanbokku@users.noreply.github.com>
2019-05-16 13:25:22 +0900

    [change] Change comments.

--- Janken/src/ui/play_ui_controller.cpp
+++ Janken/src/ui/play_ui_controller.cpp
@@ -7,18 +7,18 @@ PlayUIController::PlayUIController(): question_font_(50, Typeface::Bold)
 
 void PlayUIController::draw() const
 {
-	// $BLdBj$rI=<((B
+	// ñ‚ëËÇï\é¶
 	String question = question_flg_ ? Question_Str[0] : Question_Str[1];
 	question_font_(question).drawAt(Window::Width() / 2, Window::Height() / 6);
 
-	// $B<j$rIA2h(B
+	// éËÇï`âÊ
 	int index = static_cast<int>(hand_);
 	Hand_Texture[index].scaled(2.0).drawAt(Window::Width() / 2, Window::Height() / 2);
 }
 
 void PlayUIController::update(const std::pair<Constants::HandGesture, bool>& question)
 {
-	// $BLdBj$N99?7(B
+	// ñ‚ëËÇÃçXêV
 	hand_ = question.first;
 	question_flg_ = question.second;
 }
